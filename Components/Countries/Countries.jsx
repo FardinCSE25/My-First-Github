e85c1ee94fd8231672c5f4c90587802484fd92a5
Fardin Ahmed <fardin.cse25@gmail.com>
2025-09-19 15:56:24 +0600

    first commit

---
+++ Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>On the goo...</h1>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Countries from '../Components/Countries/Countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
